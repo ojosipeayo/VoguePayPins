afb850bf87327f20be78b236117c0ad126b8ed90
ojosipeayo <ojosipeayo@gmail.com>
2016-05-12 10:37:37 +0100

    Update VoguePayPins.php

--- VoguePayPins.php
+++ VoguePayPins.php
@@ -4,7 +4,7 @@
  */
 /*
 Plugin Name: Vogue PIN Dispenser
-Plugin URI: http://http://compisoftng.com/voguepay-woocommerce-payment-gateway
+Plugin URI: http://wordpress.com/pin-dispenser-for-voguepay
 Description: Vogue PIN Dispenser allows you integrate a PIN Dispensing system into your wordpress while pins are loaded from your voguepay account.Get card payment via mastercard,visa.
 Author: Ojosipe Ayomikun
 Author URI: http://compisoftng.com/me
@@ -28,8 +28,7 @@ if (!class_exists("VoguePayPins")) {
       $VoguePay_ButtonSettings = array( 
           'button_color'      => 'blue',
           'alternate_button'      => '',
-          'merchant_id' => '',
-          'product_type' => 'pin'
+          'merchant_id' => ''
         );
 
       $v_Options = get_option( $this->Butt_Settings );
@@ -47,7 +46,7 @@ if (!class_exists("VoguePayPins")) {
         if (isset($_POST['button_color'])) $v_Options['button_color'] = $_POST['button_color'];
     if (isset($_POST['alternate_button'])) $v_Options['alternate_button'] = $_POST['alternate_button'];
         if (isset($_POST['merchant_id'])) $v_Options['merchant_id'] = $_POST['merchant_id'];
-        if (isset($_POST['product_type'])) $v_Options['product_type'] = $_POST['product_type'];
+       
 
 
       update_option($this->Butt_Settings, $v_Options);
@@ -60,7 +59,7 @@ if (!class_exists("VoguePayPins")) {
 
       <div class=wrap>
         <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
-          <h2>Vogue Pay Pin Dispenser</h2>
+          <h2>Vogue Pin Dispenser</h2>
           <h3>Your VoguePay Merchant ID</h3>
           <p>Enter your VoguePay Merchant ID, find it on the top right hand corner when you login to voguepay.com.</p>
           <input type="text" size="50" name="merchant_id" id="merchant_id" value="<?php _e(apply_filters('format_to_edit',$v_Options['merchant_id']), 'VoguePayPins'); ?>" />
@@ -75,14 +74,6 @@ if (!class_exists("VoguePayPins")) {
             <option value="grey"  <?php if ($v_Options['button_color'] == "grey") { _e('selected="selected"', "VoguePayPins"); }?>>Grey</option>
           </select>
 
-          <h3>Product Type</h3>
-          <div>Select the product type.</div>
-          <div>
-          <select name="product_type" id="product_type">
-            <option value="pin" <?php if ($v_Options['product_type'] == "pin") { _e('selected="selected"', "VoguePayPins"); }?> >Pin Product</option>
-            <option value="downloadable" <?php if ($v_Options['product_type'] == "downloadable") { _e('selected="selected"', "VoguePayPins"); }?> >Downloadable Product</option> 
-          </select>
-          </div></br>
           <div>Or enter the url of your image to use as  "Buy Now" button.</div>
           <div> <input type="text" size="50" name="alternate_button" id="alternate_button" value="<?php _e(apply_filters('format_to_edit',$v_Options['alternate_button']), 'VoguePayPins'); ?>" />
           </div>         
@@ -99,7 +90,6 @@ if (!class_exists("VoguePayPins")) {
       extract( shortcode_atts( array( 'code' => '', 'item' => '' ), $p )); 
       $x = $this->getButtSettings(); 
       $p['merchant_id']  = $x['merchant_id'];
-      $p['product_type'] = $x['product_type'];
       $p['alternate_button'] = empty($x['alternate_button']) ? 'http://voguepay.com/images/buttons/buynow_'.$x['button_color'].'.png' : $x['alternate_button'];
       return $this->makeButtShow( $p );
     }  
@@ -114,11 +104,9 @@ if (!class_exists("VoguePayPins")) {
     <input type="hidden" name="v_merchant_id" value="'.$param['merchant_id'].'" />
     <input type="hidden" name="memo" value="Payment for '.$param['item'].'" />';
 
-    if ( $param['product_type'] == 'pin' ) $form .= '<input type="hidden" name="xid_code" value="'.$param['code'].'" />';
-     if ( $param['product_type'] == 'downloadable' ) $form .= '<input type="hidden" name="vid_code" value="'.$param['code'].'" />';
-
-    $form .= '<input type="hidden" name="developer_code" value="56e0022f80a0c" />
-    <input type="image" style="border: 0;" name="submit" src="'.$param['alternate_button'].'" alt="Pay with VoguePay" />
+     $form .= '<input type="hidden" name="xid_code" value="'.$param['code'].'" />';
+     $form .= '<input type="hidden" name="developer_code" value="56e0022f80a0c" />
+     <input type="image" style="border: 0;" name="submit" src="'.$param['alternate_button'].'" alt="Pay     with VoguePay" />
     </form>';
 
     return $form;
@@ -134,7 +122,7 @@ if ( !function_exists("VoguePayPins_ap") ) {
     function VoguePayPins_ap() { 
         global $v_VoguePayPins; 
         if ( !isset($v_VoguePayPins) )return; 
-        if ( function_exists('add_options_page') )  add_options_page('Vogue Pay PIN Dispenser', 'Vogue Pay PIN Dispenser', 9, basename(__FILE__), array(&$v_VoguePayPins, 'echoSettingPage'));
+        if ( function_exists('add_options_page') )  add_options_page('Vogue PIN Dispenser', 'Vogue PIN Dispenser', 9, basename(__FILE__), array(&$v_VoguePayPins, 'echoSettingPage'));
     }   
 }
 
